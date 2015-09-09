cc8b4d7fa0e32c1e8a33ac5437c47c80395a3041
Joshua Simpson <joshwvs@gmail.com>
2015-09-09 17:31:18 -0400

    Break up long file path.

--- code/src/main/scala/SimpleApp.scala
+++ code/src/main/scala/SimpleApp.scala
@@ -6,6 +6,8 @@ import org.apache.spark.SparkConf
 object WeblogChallenge {
   def main(args: Array[String]) {
 
+    val projectRoot = "/home/josh/Career/technical_questions/WeblogChallenge"
+    val logFile = projectRoot + "/data/2015_07_22_mktplace_shop_web_log_sample.log.gz"
     val conf = new SparkConf().setAppName("Weblog Challenge")
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
