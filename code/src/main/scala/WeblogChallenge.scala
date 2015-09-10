87c0f649473cd000b8201ac6c136e993f854f1d0
Joshua Simpson <joshwvs@gmail.com>
2015-09-10 10:29:29 -0400

    Group timestamps by IP address.
    Aha, here's the function I was looking for. groupByKey will give us a
    collection of all the time stamps for each IP address. We should be able
    to use this (after sorting?) to iterate through and build the sessions.
    (At each step, check if the new record is within $TIMEOUT of the old
    one).
    
    Prior to this, I had been scratching my head trying to write a reducer
    (passed to reduceByKey) that would somehow get the job done without
    relying on any execution ordering.

--- code/src/main/scala/WeblogChallenge.scala
+++ code/src/main/scala/WeblogChallenge.scala
@@ -14,11 +14,18 @@ object WeblogChallenge {
     val logData = sc.textFile(logFile, 2).cache()
 
     // For each line (i.e. log entry), extract IP and timestamp
-    logData.map(line => {
-        val logEntry = line.split(" ") // per AWS documentation: space-delimited fields (consistent order)
-        val timestamp = new DateTime(logEntry(0))
-        // for each line, output (IP, (time, time)) (first is start time, second is end time)
-        (logEntry(2).split(":")(0), (timestamp, timestamp)) // drop the port from the IP
-    }).reduceByKey((a, b) => if (a > b) (b, a) else (a, b)).take(10).foreach(println)
+    val users = logData.map(line => {
+            val logEntry = line.split(" ") // per AWS documentation: space-delimited fields (consistent order)
+            val timestamp = new DateTime(logEntry(0))
+            // for each line, output (IP, (time, time)) (first is start time, second is end time)
+            (logEntry(2).split(":")(0), (timestamp, timestamp)) // drop the port from the IP
+        }).groupByKey()
+
+    // thin slice: we should (hopefully) see a collection of our (ts, ts) pairs here
+    println(users.take(10)(7))
+
+    // Now, for each IP we should be able to sort and then iterate through their accesses. For each
+    // record, check if it is within $SESSION_TIMEOUT of the last one: if so, continue this
+    // session; else start a new one.
   }
 }
